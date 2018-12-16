330de238c8bd914b074f2d7d45c46278732fa6aa
alevallarta <45907264+alevallarta@users.noreply.github.com>
2018-12-15 23:48:40 -0600

    navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+$('.nav-link').click(function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click(); //bootstrap 3.x by Richard
+    $('.navbar-toggler').click(); //bootstrap 4.x
+});
